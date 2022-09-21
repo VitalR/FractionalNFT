5aa3f091ee49edbdccf41d3e21ab2bfb9b03eac7
VitalR <vitalii.rodikov@gmail.com>
2022-09-21 20:15:45 +0300

    Added mintable ERC721

--- script/FERC721.s.sol
+++ script/FERC721.s.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
 
 import "forge-std/Script.sol";
 
-contract ContractScript is Script {
+contract FERC721 is Script {
     function setUp() public {}
 
     function run() public {
--- test/FERC721.t.sol
+++ test/FERC721.t.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
 
 import "forge-std/Test.sol";
 
-contract ContractTest is Test {
+contract FERC721 is Test {
     function setUp() public {}
 
     function testExample() public {
