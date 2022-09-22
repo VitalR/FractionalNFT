ab6d635764f42738354f83649d0d981ab877c5fd
VitalR <vitalii.rodikov@gmail.com>
2022-09-22 21:16:48 +0300

    Added smoke test

--- test/FERC721.t.sol
+++ test/FERC721.t.sol
@@ -2,11 +2,29 @@
 pragma solidity ^0.8.14;
 
 import "forge-std/Test.sol";
+import "src/FERC721.sol";
 
-contract FERC721 is Test {
-    function setUp() public {}
+contract FERC721Test is Test {
 
-    function testExample() public {
-        assertTrue(true);
+    address owner;
+    address ZERO_ADDRESS = address(0);
+    address user = address(1);
+    uint256 public royaltyValue = 250;
+
+    string name = "TokenName";
+    string symbol = "TNS";
+
+    FERC721 public nft;
+
+    function setUp() public {
+        owner = address(this);
+        nft = new FERC721(name, symbol);
+    }
+
+    function testMintToWorks() public {
+        vm.prank(user);
+        nft.mintTo(address(user));
+        uint256 userBalance = nft.balanceOf(user);
+        assertEq(userBalance, 1);
     }
 }
