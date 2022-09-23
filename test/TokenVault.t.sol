74ea4c412a41aa0903a1902f7f481c19dc1aa3c7
VitalR <vitalii.rodikov@gmail.com>
2022-09-23 20:16:21 +0300

    Add more tests

--- test/TokenVault.t.sol
+++ test/TokenVault.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.14;
 
 import "forge-std/Test.sol";
 import "src/TokenVault.sol";
+import "src/FERC721.sol";
 
 contract TokenVaultTest is Test {
     address owner;
@@ -10,14 +11,19 @@ contract TokenVaultTest is Test {
     address curator = address(1);
     address user = address(2);
     uint fee = 0;
+    uint tokenId = 1;
+    uint supply = 1000;
+    uint price = 0.1 ether;
 
     string name = "TokenName";
     string symbol = "TNS";
 
+    FERC721 public collection;
     TokenVault public tokenVault;
 
     function setUp() public {
         owner = address(this);
+        collection = new FERC721(name, symbol);
         tokenVault = new TokenVault(curator, fee, name, symbol);
     }
 
@@ -27,5 +33,39 @@ contract TokenVaultTest is Test {
         assertEq(tokenVault.decimals(), 0);
         assertEq(tokenVault.curator(), curator);
         assertEq(tokenVault.fee(), fee);
+    }
+
+    function testFractionalizedWorksAsOwner() public {
+        vm.prank(owner);
+        collection.mintTo(address(tokenVault));
+        assertEq(collection.balanceOf(address(tokenVault)), 1);
+        tokenVault.fractionalize(address(tokenVault), address(collection), tokenId, supply);
+        assertEq(tokenVault.totalSupply(), supply);
+        assertEq(tokenVault.balanceOf(address(curator)), supply);
+    }
+
+    function testFractionalizedFailsAsNotOwner() public {
+        vm.prank(owner);
+        collection.mintTo(address(tokenVault));
+        assertEq(collection.balanceOf(address(tokenVault)), 1);
+        vm.prank(user);
+        vm.expectRevert("Ownable: caller is not the owner");
+        tokenVault.fractionalize(address(tokenVault), address(collection), tokenId, supply);
+        assertEq(tokenVault.totalSupply(), 0);
+        assertEq(tokenVault.balanceOf(address(curator)), 0);
+    }
+
+    function testConfigureSaleWorksAsOwner() public {
+        vm.prank(owner);
+        collection.mintTo(address(tokenVault));
+        assertEq(collection.balanceOf(address(tokenVault)), 1);
+        tokenVault.fractionalize(address(tokenVault), address(collection), tokenId, supply);
+        assertEq(tokenVault.totalSupply(), supply);
+        assertEq(tokenVault.balanceOf(address(curator)), supply);
+        uint start = block.timestamp + 1;
+        tokenVault.configureSale(start, 0, price);
+        assertEq(tokenVault.start(), start);
+        assertEq(tokenVault.end(), 0);
+        assertEq(tokenVault.listPrice(), price);
     }
 }
