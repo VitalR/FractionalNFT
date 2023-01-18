b6a976885b61f391a48e1a966a1fe87b80c7e8a7
VitalR <vitalii.rodikov@gmail.com>
2023-01-19 01:34:15 +0200

    Factory implemented

--- src/VaultFactory.sol
+++ src/VaultFactory.sol
@@ -2,44 +2,34 @@
 pragma solidity ^0.8.14;
 
 import "openzeppelin-contracts/access/Ownable.sol";
-// import "openzeppelin-contracts/token/ERC721/IERC721.sol";
+import "openzeppelin-contracts/security/Pausable.sol";
+import { CREATE3 } from "lib/solmate/src/utils/CREATE3.sol";
 import "./TokenVault.sol";
 
-import { CREATE3 } from "lib/solmate/src/utils/CREATE3.sol";
-
-import "forge-std/console.sol";
-
-// interface IERC721 {
-//     function safeTransferFrom(
-//         address from,
-//         address to,
-//         uint256 tokenId
-//     ) external;
-// }
-
-interface ITokenVault {
-    function initialize(address, uint) external;
-    function fractionalize(address, uint, uint) external;
-}
-
-// Pausable
-
-contract VaultFactory is Ownable {
+contract VaultFactory is Ownable, Pausable {
     /// @notice the number of vaults
     uint public vaultCount;
 
     /// @notice the mapping of vault number to vault contract
     mapping(uint => address) public vaults;
-    address[] public allVaults;
 
-    address public logic;
-
+    /// @notice Emitted when a new NFT vault is deployed.
     event VaultCreated(address collection, uint tokenId, address vault, uint vaultCount);
 
-    constructor() {} 
+    constructor() {}
 
-
-    // create and deploy vault
+    /// @notice the function to create and deploy a new vault
+    /// @param _collection the ERC721 token address fo the NFT
+    /// @param _tokenId the uint ID of the token
+    /// @param _supply the total supply amount of fractions of the fractionalized NFT
+    /// @param _fee the platform fee paid to the curator
+    /// @param _name the desired name of the vault
+    /// @param _symbol the desired sumbol of the vault
+    /// @param _uri the URI for all tokens
+    /// @param _start The start date of primary sale
+    /// @param _end The end date of primary sale
+    /// @param _price the initial price of the NFT
+    /// @return address of the vault
     function createVault(
         address _collection,
         uint _tokenId,
@@ -51,7 +41,7 @@ contract VaultFactory is Ownable {
         uint _start,
         uint _end,
         uint _price
-    ) public returns (address) {
+    ) public whenNotPaused returns (address) {
         bytes32 salt = keccak256(abi.encodePacked(
             _collection,
             _tokenId,
@@ -92,12 +82,17 @@ contract VaultFactory is Ownable {
 
         vaultCount++;
         vaults[vaultCount] = vaultAddress;
-        allVaults.push(vaultAddress);
-
 
         emit VaultCreated(_collection, _tokenId, vaultAddress, vaultCount);
 
         return vaultAddress;
     }
 
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
 }
--- test/VaultFactory.t.sol
+++ test/VaultFactory.t.sol
@@ -6,8 +6,8 @@ import "src/TokenVault.sol";
 import "src/NFTCollection.sol";
 import "src/VaultFactory.sol";
 
-import "test/mocks/MockFractionalNFT.sol";
-import "src/FractionalNFT.sol";
+// import "test/mocks/MockFractionalNFT.sol";
+// import "src/FractionalNFT.sol";
 
 import "forge-std/console.sol";
 
@@ -27,24 +27,20 @@ contract VaultFactoryTest is Test {
     string tokenUri = "tokenURI";
 
     address curator = address(11);
+    address user = address(12);
 
     function setUp() public {
         collection = new NFTCollection();
         factory = new VaultFactory();
     }
 
-    function mintNft() public {
-        collection.mint(
-            address(curator),
-            tokenUri,
-            address(curator),
-            250
-        );
+    function mintNft(address to) public {
+        collection.mint(address(to), tokenUri, address(to), 250);
         assertEq(collection.balanceOf(address(curator)), 1);
     }
 
     function testCreateTokenVault() public {
-        mintNft();
+        mintNft(address(curator));
         assertEq(collection.ownerOf(tokenId), address(curator));
         vm.startPrank(curator);
         collection.approve(address(factory), tokenId);
@@ -64,6 +60,7 @@ contract VaultFactoryTest is Test {
             end,
             price
         );
+        vm.stopPrank();
 
         assertEq(factory.vaultCount(), 1);
         assertEq(factory.vaults(1), address(vault));
@@ -76,4 +73,83 @@ contract VaultFactoryTest is Test {
         assertEq(TokenVault(vault).balanceOf(address(curator)), supply);
     }
 
+    function testCreateSeveralTokenVaults() public {
+        testCreateTokenVault();
+
+        uint tokenId2 = 2;
+
+        collection.mint(address(user), tokenUri, address(user), 250);
+
+        assertEq(collection.ownerOf(tokenId2), address(user));
+        vm.startPrank(user);
+        collection.approve(address(factory), tokenId2);
+        
+        uint start = block.timestamp + 1;
+        uint end = 0;
+        
+        address vault = VaultFactory(factory).createVault(
+            address(collection),
+            tokenId2,
+            supply,
+            fee,
+            name,
+            symbol,
+            tokenUri,
+            start,
+            end,
+            price
+        );
+        vm.stopPrank();
+
+        assertEq(factory.vaultCount(), 2);
+        assertEq(factory.vaults(2), address(vault));
+
+        assertEq(collection.balanceOf(address(vault)), 1);
+        assertEq(collection.ownerOf(tokenId2), address(vault));
+
+        assertEq(TokenVault(vault).curator(), address(user));
+        assertEq(TokenVault(vault).name(), name);
+        assertEq(TokenVault(vault).symbol(), symbol);
+        assertEq(TokenVault(vault).uri(0), tokenUri);
+        assertEq(TokenVault(vault).totalSupply(), supply);
+        assertEq(TokenVault(vault).balanceOf(address(user)), supply);
+
+        assertEq(TokenVault(vault).start(), start);
+        assertEq(TokenVault(vault).end(), end);
+        assertEq(TokenVault(vault).listPrice(), price);
+    }
+
+    function testUnableCreateVaultIfPaused() public {
+        factory.pause();
+
+        mintNft(address(curator));
+        vm.startPrank(curator);
+        collection.approve(address(factory), tokenId);
+
+        uint start = block.timestamp + 1;
+        uint end = 0;
+
+        vm.expectRevert("Pausable: paused");
+        VaultFactory(factory).createVault(
+            address(collection),
+            tokenId,
+            supply,
+            fee,
+            name,
+            symbol,
+            tokenUri,
+            start,
+            end,
+            price
+        );
+        vm.stopPrank();
+    }
+
+    function testUnableUnpausedNotOwner() public {
+        testUnableCreateVaultIfPaused();
+        vm.startPrank(curator);
+        vm.expectRevert("Ownable: caller is not the owner");
+        factory.unpause();
+        vm.stopPrank();
+    }
 }
